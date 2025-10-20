b501cf2e3434de8d7b510dffc4febfa3c7cd07ac
Harout Mardirossian <harmardir@gmail.com>
2025-10-20 10:01:07 +0400

    fixing styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,9 +10,9 @@ import linkedinIcon from './linkedin.png';
 import youtubeIcon from './youtube.png';
 import tiktokIcon from './tiktok.png';
 import snapchatIcon from './snapchat.png';
-import logo from './sbmf-logo.png';
+import logo from './logo.png';
 
-import '../_footer.scss';
+import '../_footer.scss'; // Correct relative path
 
 ensureConfig(['LMS_BASE_URL'], 'Footer component');
 
@@ -45,41 +45,29 @@ class Footer extends React.Component {
 
           {/* === Top Section === */}
           <div className="footer-top">
-             <div className="footer-brand">
-    <div className="footer-title">
-      <h2>مهارات المستقبل</h2>
-    </div>
-    <img src={logo} alt="Logo" className="footer-logo" />
-  </div>
+            <div className="footer-brand">
+              <div className="footer-title">
+                <h2>مهارات المستقبل</h2>
+                <p>مؤسسة سالم بن محفوظ الأهلية<br />Salem Bin Mahfouz Foundation</p>
+              </div>
+              <img src={logo} alt="Logo" className="footer-logo" />
+            </div>
 
-  <div className="footer-links-top">
-    <a
-      href={`${config.LMS_BASE_URL}/privacy`}
-      className="footer-link"
-      onClick={this.externalLinkClickHandler}
-    >
-      سياسة الخصوصية
-    </a>
-    <a
-      href={`${config.LMS_BASE_URL}/tos`}
-      className="footer-link"
-      onClick={this.externalLinkClickHandler}
-    >
-      شروط الخدمة
-    </a>
-  </div>
+            <div className="footer-links-top">
+              <a href="#" className="footer-link" onClick={this.externalLinkClickHandler}>الأسئلة الشائعة</a>
+              <a href="#" className="footer-link" onClick={this.externalLinkClickHandler}>تواصل معنا</a>
+              <a href="#" className="footer-link" onClick={this.externalLinkClickHandler}>البريد الإلكتروني</a>
+            </div>
           </div>
 
           {/* Divider */}
           <div className="footer-divider" />
 
           {/* === Bottom Section === */}
-      <div className="footer-bottom">
-
-        <div className="footer-links-bottom">
-              <a href="#" className="footer-link" onClick={this.externalLinkClickHandler}>الأسئلة الشائعة</a>
-              <a href="#" className="footer-link" onClick={this.externalLinkClickHandler}>تواصل معنا</a>
-              <a href="#" className="footer-link" onClick={this.externalLinkClickHandler}>البريد الإلكتروني</a>
+          <div className="footer-bottom">
+            <div className="footer-links-bottom">
+              <a href={`${config.LMS_BASE_URL}/privacy`} className="footer-link" onClick={this.externalLinkClickHandler}>سياسة الخصوصية</a>
+              <a href={`${config.LMS_BASE_URL}/tos`} className="footer-link" onClick={this.externalLinkClickHandler}>شروط الخدمة</a>
             </div>
 
             <div className="footer-social">
@@ -91,14 +79,12 @@ class Footer extends React.Component {
               <a href="#" target="_blank" rel="noopener noreferrer"><img src={tiktokIcon} alt="TikTok" /></a>
               <a href="#" target="_blank" rel="noopener noreferrer"><img src={snapchatIcon} alt="Snapchat" /></a>
             </div>
- 
-</div>
+          </div>
 
           {/* Copyright */}
           <div className="footer-copy">
             <p>معلومات حقوق النشر تعود لمؤسسة سالم بن محفوظ الأهلية ©</p>
           </div>
-
         </div>
       </footer>
     );
